337099e59c0aa511cf9487db418e02c12096bef1
Dilshad <dilshadahmad92425@gmail.com>
2023-08-04 21:12:22 +0530

    ...

--- app/page.tsx
+++ app/page.tsx
@@ -29,13 +29,13 @@ export const dynamic = 'force-dynamic';
 export const dynamicParams = true;
 export const revalidate = 0;
 
-const Home = async ({ searchParams: { category, endcursor } }: Props) => {
+const Home = async ({ searchParams: { category = null, endcursor } }: Props) => {
 
-   // Check if the category is not provided or is not a string
-  if (typeof category !== "string") {
-    // Set category to null to indicate that all projects should be fetched
-    category = null;
-  }
+  //  // Check if the category is not provided or is not a string
+  // if (typeof category !== "string") {
+  //   // Set category to null to indicate that all projects should be fetched
+  //   category = null;
+  // }
 
   const data = await fetchAllProjects(category, endcursor) as ProjectSearch
 
